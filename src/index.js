246ea9ad6141874e1c64c729ecb34517c932dae2
Eastwood11 <1453014743@qq.com>
2020-02-26 19:33:37 +0800

    tabBar导入

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
+import { TabBar } from 'antd-mobile'
+import 'antd-mobile/dist/antd-mobile.css'
 import './index.css'
 
 ReactDOM.render(<App />,document.getElementById('root'))
